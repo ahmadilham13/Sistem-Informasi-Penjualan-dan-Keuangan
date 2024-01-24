94dc26a1254dc330d44814890d1684ac05400c3a
ahmadilham_13 <ahmad13ilham13@gmail.com>
2024-01-24 15:14:54 +0700

    Update: UI layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,204 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- flatpickr style -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <!-- flatpickr style -->
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+    <!-- Styles -->
+    @vite(['resources/css/app.css'])
 
-            <!-- Page Content -->
-            <main>
+    <!-- flatpickr style -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <!-- flatpickr style -->
+
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+
+    @routes
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+    <!-- Scripts -->
+</head>
+
+<body class="font-sans antialiased">
+    <div x-data="{ menuOpen: false }" class="flex min-h-screen custom-scrollbar">
+        <!-- start::Black overlay -->
+        <div :class="menuOpen ? 'block' : 'hidden'" @click="menuOpen = false"
+            class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
+        <!-- end::Black overlay -->
+        <!-- start::Sidebar -->
+        @include('layouts.sidebar')
+        <!-- end::Sidebar -->
+        <div class="flex flex-col w-full lg:pl-64">
+            <!-- start::Topbar -->
+            @include('layouts.header')
+            <!-- end::Topbar -->
+            <!-- start:Page content -->
+            <div class="h-full px-5 py-8 bg-gray-200">
                 {{ $slot }}
-            </main>
+            </div>
+            <!-- end:Page content -->
         </div>
-    </body>
+    </div>
+    <script>
+        // Stats by category
+        document.addEventListener("alpine:init", () => {
+            Alpine.data("statsByCategory", () => ({
+                items: [{
+                    name: "Project 1",
+                    percent: "71",
+                },
+                {
+                    name: "Project 2",
+                    percent: "63",
+                },
+                {
+                    name: "Project 3",
+                    percent: "92",
+                },
+                {
+                    name: "Project 4",
+                    percent: "84",
+                },
+                ],
+                currentItem: {
+                    name: "Project 1",
+                    percent: "71",
+                },
+            }));
+        });
+
+        // Project overview stats
+        document.addEventListener("alpine:init", () => {
+            Alpine.data("productOverviewStats", () => ({
+                project: {
+                    completed: 149,
+                    in_progress: 42,
+                },
+            }));
+        });
+
+        // start::Chart 1
+        const labels = [
+            "January",
+            "February",
+            "Mart",
+            "April",
+            "May",
+            "Jun",
+            "July",
+        ];
+
+        const data_1 = {
+            labels: labels,
+            datasets: [{
+                label: "My First Dataset",
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                    "rgba(255, 205, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(201, 203, 207, 0.2)",
+                ],
+                borderColor: [
+                    "rgb(255, 99, 132)",
+                    "rgb(255, 159, 64)",
+                    "rgb(255, 205, 86)",
+                    "rgb(75, 192, 192)",
+                    "rgb(54, 162, 235)",
+                    "rgb(153, 102, 255)",
+                    "rgb(201, 203, 207)",
+                ],
+                borderWidth: 1,
+            },],
+        };
+
+        const config_1 = {
+            type: "bar",
+            data: data_1,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        };
+
+        var chart_1 = new Chart(document.getElementById("chart_1"), config_1);
+
+        // end::Chart 1
+
+        // start::Chart 2
+        const data_2 = {
+            labels: [
+                "Eating",
+                "Drinking",
+                "Sleeping",
+                "Designing",
+                "Coding",
+                "Cycling",
+                "Running",
+            ],
+            datasets: [{
+                label: "My First Dataset",
+                data: [65, 59, 90, 81, 56, 55, 40],
+                fill: true,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgb(255, 99, 132)",
+                pointBackgroundColor: "rgb(255, 99, 132)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgb(255, 99, 132)",
+            },
+            {
+                label: "My Second Dataset",
+                data: [28, 48, 40, 19, 96, 27, 100],
+                fill: true,
+                backgroundColor: "rgba(54, 162, 235, 0.2)",
+                borderColor: "rgb(54, 162, 235)",
+                pointBackgroundColor: "rgb(54, 162, 235)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgb(54, 162, 235)",
+            },
+            ],
+        };
+
+        const config_2 = {
+            type: "radar",
+            data: data_2,
+            options: {
+                elements: {
+                    line: {
+                        borderWidth: 3,
+                    },
+                },
+            },
+        };
+
+        var chart_2 = new Chart(document.getElementById("chart_2"), config_2);
+        // end::Chart 2
+    </script>
+    @stack('scripts')
+</body>
+
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,72 @@
+<aside :class="menuOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed inset-y-0 left-0 z-10 w-64 overflow-y-auto transition duration-300 bg-slate-500 lg:translate-x-0 lg:inset-0 custom-scrollbar">
+    <!-- start::Logo -->
+    <div class="flex items-center justify-center h-16 bg-black bg-opacity-30">
+      <h1 class="text-lg font-bold tracking-widest text-gray-100 uppercase">
+        Rido Flowers
+      </h1>
+    </div>
+    <!-- end::Logo -->
+  
+    <!-- start::Navigation -->
+    <nav class="py-10 custom-scrollbar">
+      <!-- start::Menu link -->
+      <a x-data="{ linkHover: false }" @mouseover="linkHover = true" @mouseleave="linkHover = false" href="{{ route(name: 'dashboard', absolute: false) }}" class="flex items-center px-6 py-3 text-gray-300 transition duration-200 cursor-pointer hover:bg-black hover:bg-opacity-30">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class="linkHover ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+        </svg>
+        <span class="ml-3 transition duration-200" :class="linkHover ? 'text-gray-100' : ''">
+          Dashboard
+        </span>
+      </a>
+      <!-- end::Menu link -->
+  
+      <!-- start::Menu link -->
+      <div x-data="{ linkHover: false, linkActive: false }">
+        <div @mouseover="linkHover = true" @mouseleave="linkHover = false" @click="linkActive = !linkActive" class="flex items-center justify-between px-6 py-3 text-gray-300 transition duration-200 cursor-pointer hover:text-gray-100 hover:bg-black hover:bg-opacity-30" :class=" linkActive ? 'bg-black bg-opacity-30 text-gray-100' : ''">
+          <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 transition duration-200" :class=" linkHover || linkActive ? 'text-gray-100' : ''" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+            </svg>
+            <span class="ml-3">Project</span>
+          </div>
+          <svg class="w-3 h-3 transition duration-300" :class="linkActive ? 'rotate-90' : ''" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+          </svg>
+        </div>
+        <!-- start::Submenu -->
+        <ul x-show="linkActive" x-cloak x-collapse.duration.300ms class="text-gray-200">
+          <!-- start::Submenu link -->
+          <li class="py-2 pl-10 pr-6 transition duration-200 cursor-pointer hover:bg-black hover:bg-opacity-30 hover:text-gray-100">
+            <a href="#" class="flex items-center">
+              <span class="mr-2 text-sm">&bull;</span>
+              <span class="overflow-ellipsis">Project</span>
+            </a>
+          </li>
+          <!-- end::Submenu link -->
+  
+          <!-- start::Submenu link -->
+          <li class="py-2 pl-10 pr-6 transition duration-200 cursor-pointer hover:bg-black hover:bg-opacity-30 hover:text-gray-100">
+            <a href="#" class="flex items-center">
+              <span class="mr-2 text-sm">&bull;</span>
+              <span class="overflow-ellipsis">Project Group</span>
+            </a>
+          </li>
+          <!-- end::Submenu link -->
+  
+          <!-- start::Submenu link -->
+          <li class="py-2 pl-10 pr-6 transition duration-200 cursor-pointer hover:bg-black hover:bg-opacity-30 hover:text-gray-100">
+            <a href="#" class="flex items-center">
+              <span class="mr-2 text-sm">&bull;</span>
+              <span class="overflow-ellipsis">Client</span>
+            </a>
+          </li>
+          <!-- end::Submenu link -->
+        </ul>
+        <!-- end::Submenu -->
+      </div>
+      <!-- end::Menu link -->
+  
+      <!-- <p class="px-6 mt-10 mb-2 text-xs text-gray-300 uppercase">Apps</p> -->
+    </nav>
+    <!-- end::Navigation -->
+  </aside>
