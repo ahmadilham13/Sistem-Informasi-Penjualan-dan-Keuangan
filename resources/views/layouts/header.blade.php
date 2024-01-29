3581e6880a88e1409f93bedc5332015650ef7e87
ahmadilham_13 <ahmad13ilham13@gmail.com>
2024-01-29 21:24:51 +0700

    Update: hide inbox and setting

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -50,7 +50,7 @@
               </a>
               <!-- end::Submenu link -->
               <!-- start::Submenu link -->
-              <a x-data="{ linkHover: false }" href="./pages/email/inbox.html" class="flex items-center justify-between px-3 py-2 hover:bg-gray-100 bg-opacity-20" @mouseover="linkHover = true" @mouseleave="linkHover = false">
+              {{-- <a x-data="{ linkHover: false }" href="./pages/email/inbox.html" class="flex items-center justify-between px-3 py-2 hover:bg-gray-100 bg-opacity-20" @mouseover="linkHover = true" @mouseleave="linkHover = false">
                 <div class="flex items-center">
                   <svg class="w-5 h-5 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
@@ -62,10 +62,10 @@
                     </p>
                   </div>
                 </div>
-              </a>
+              </a> --}}
               <!-- end::Submenu link -->
               <!-- start::Submenu link -->
-              <a x-data="{ linkHover: false }" href="./pages/settings.html" class="flex items-center justify-between px-3 py-2 hover:bg-gray-100 bg-opacity-20" @mouseover="linkHover = true" @mouseleave="linkHover = false">
+              {{-- <a x-data="{ linkHover: false }" href="./pages/settings.html" class="flex items-center justify-between px-3 py-2 hover:bg-gray-100 bg-opacity-20" @mouseover="linkHover = true" @mouseleave="linkHover = false">
                 <div class="flex items-center">
                   <svg class="w-5 h-5 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
@@ -77,7 +77,7 @@
                     </p>
                   </div>
                 </div>
-              </a>
+              </a> --}}
               <!-- end::Submenu link -->
   
               <hr />
